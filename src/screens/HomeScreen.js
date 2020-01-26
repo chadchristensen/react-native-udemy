38007b8920743119023bfbc15f62fa0280210f8b
Chad Christensen <chadchristensen2@gmail.com>
2020-01-26 09:45:21 -0500

    Import screen componenets to Home and use App for routing

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,17 +1,45 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { Text, StyleSheet, View, Button } from 'react-native';
+import PropTypes from 'prop-types';
 
-const HomeScreen = ({ navigation }) => {
-  return (
-    <View>
-      <Text style={{ fontSize: 30 }}>HomeScreen</Text>
-      <Button title="Layout" onPress={() => navigation.navigate('Layout')} />
-    </View>
-  );
-};
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 30,
+  },
+});
 
-HomeScreen.navigationOptions = {
-  title: 'Home'
+const HomeScreen = ({ navigation }) => (
+  <View>
+    <Text style={styles.text}>Hi Chad</Text>
+    <Button
+      onPress={() => navigation.navigate('Components')}
+      title="Go to Components Demo"
+    />
+    <Button
+      onPress={() => navigation.navigate('List')}
+      title="Go to List Demo"
+    />
+    <Button
+      onPress={() => navigation.navigate('Image')}
+      title="Go to Image Demo"
+    />
+    <Button
+      onPress={() => navigation.navigate('Counter')}
+      title="Go to Counter Demo"
+    />
+    <Button
+      onPress={() => navigation.navigate('Color')}
+      title="Go to Color Demo"
+    />
+    <Button
+      onPress={() => navigation.navigate('Square')}
+      title="Go to Square Demo"
+    />
+  </View>
+);
+
+HomeScreen.propTypes = {
+  navigation: PropTypes.object,
 };
 
 export default HomeScreen;
